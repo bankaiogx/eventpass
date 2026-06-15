30891edd50685902c9af1fd35f84f7ac67d179e4
bankaiogx <ayazarbaz71@gmail.com>
2026-06-16 00:25:24 +0100

    Customise admin for EventPass models

--- events/admin.py
+++ events/admin.py
@@ -3,6 +3,24 @@ from django.contrib import admin
 from .models import Category, Event, Venue
 
 
-admin.site.register(Category)
-admin.site.register(Venue)
-admin.site.register(Event)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+    prepopulated_fields = {"slug": ("name",)}
+    search_fields = ("name",)
+
+
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+    list_display = ("name", "city", "postcode")
+    search_fields = ("name", "city", "postcode")
+    list_filter = ("city",)
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "venue", "start_date", "start_time", "is_published")
+    list_filter = ("category", "venue", "is_published", "start_date")
+    search_fields = ("title", "description", "venue__name", "venue__city")
+    list_editable = ("is_published",)
+    ordering = ("start_date", "start_time")
--- tickets/admin.py
+++ tickets/admin.py
@@ -3,6 +3,23 @@ from django.contrib import admin
 from .models import Order, OrderItem, TicketType
 
 
-admin.site.register(TicketType)
-admin.site.register(Order)
-admin.site.register(OrderItem)
+@admin.register(TicketType)
+class TicketTypeAdmin(admin.ModelAdmin):
+    list_display = ("event", "name", "price", "quantity_available", "sale_active")
+    list_filter = ("sale_active", "event")
+    search_fields = ("event__title", "name")
+    list_editable = ("price", "quantity_available", "sale_active")
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "event", "total_amount", "payment_status", "created_at")
+    list_filter = ("payment_status", "created_at", "event")
+    search_fields = ("user__username", "user__email", "event__title", "stripe_checkout_id")
+    readonly_fields = ("created_at",)
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ("order", "ticket_type", "quantity", "price_at_purchase")
+    search_fields = ("order__user__username", "ticket_type__name", "ticket_type__event__title")
