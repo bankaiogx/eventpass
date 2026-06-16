0296d9c4f5ee3b16cad3c36264a1cc0613c7180c
bankaiogx <ayazarbaz71@gmail.com>
2026-06-16 20:16:52 +0100

    Style register form fields

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import User
 
 
 class RegisterForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({"class": "form-control"})
+
     class Meta:
         model = User
         fields = ["username", "password1", "password2"]
