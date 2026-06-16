2b64b1727251402876b12539af14cd6c8a826d4f
bankaiogx <ayazarbaz71@gmail.com>
2026-06-16 18:39:46 +0100

    Connect register URL

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
-from django.views.generic import TemplateView
+
+from . import views
 
 
 urlpatterns = [
-    path("register/", TemplateView.as_view(template_name="accounts/register.html"), name="register"),
+    path("register/", views.register, name="register"),
     path("login/", auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
