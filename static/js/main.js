29761ba6c6791b4eae2b93a610404a8e5372a842
bankaiogx <ayazarbaz71@gmail.com>
2026-06-16 01:04:46 +0100

    Add card hover JavaScript

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,9 @@
-// Public site JavaScript will be added in later tasks.
+const cards = document.querySelectorAll(".event-card");
+
+cards.addEventListener("mouseenter", function () {
+    this.style.transform = "scale(1.03)";
+});
+
+cards.addEventListener("mouseleave", function () {
+    this.style.transform = "scale(1)";
+});
