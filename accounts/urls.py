0209fc42ad83cf66532976d1b1f3b8fbd93ae1f3
bankaiogx <ayazarbaz71@gmail.com>
2026-06-16 20:29:37 +0100

    Add my tickets URL

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     path("login/", auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("my-tickets/", views.my_tickets, name="my_tickets"),
 ]
