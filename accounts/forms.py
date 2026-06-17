0a92c021bbcd59b02ce3c84e809353f6d6d90b1a
bankaiogx <ayazarbaz71@gmail.com>
2026-06-17 23:57:51 +0100

    ask for email on register

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,14 +6,24 @@ from .models import SupportRequest
 
 
 class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.widget.attrs.update({"class": "form-control"})
 
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("An account with this email already exists.")
+
+        return email
+
     class Meta:
         model = User
-        fields = ["username", "password1", "password2"]
+        fields = ["username", "email", "password1", "password2"]
 
 
 class LoginForm(AuthenticationForm):
