b09963528672452980363bc48b69dd91d3caa97c
bankaiogx <ayazarbaz71@gmail.com>
2026-06-18 14:34:09 +0100

    refresh card animation files

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,3 @@
-const cards = document.querySelectorAll(".event-card");
-
-cards.forEach(function (card) {
-    card.addEventListener("mouseenter", function () {
-        this.style.transform = "scale(1.03)";
-    });
-
-    card.addEventListener("mouseleave", function () {
-        this.style.transform = "scale(1)";
-    });
-});
-
 const ticketInputs = document.querySelectorAll(".ticket-quantity");
 const bookingTotal = document.getElementById("booking-total");
 const selectedTicketCount = document.getElementById("selected-ticket-count");
